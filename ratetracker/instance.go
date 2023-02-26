28cab94b3b22d11abf21c7ef207c290476ebb745
David He <david@Davids-MacBook-Air.local>
2023-02-26 01:08:51 -0800

    added comments for jaser

--- ratetracker/instance.go
+++ ratetracker/instance.go
@@ -4,3 +4,13 @@ type RateTrackerInstance struct {
 	
 }
 
+// TODO: Jaser
+// when instantiating this struct, we want to "find" all ratelimite_id,
+// create N linked list sliding window (see ds.go)
+// then create our min heap, and add N sliding window to min heap.
+// this will all be done in init
+// this struct should also have funcs and store data for below
+// 1. get total requests in all RL
+// 2. perform tn - t1 calculation
+// 3. check logic whether or not tn can be added, return success/fail
+// 4. ... whatever else u think is good
--- ratetracker/ratetracker.go
+++ ratetracker/ratetracker.go
@@ -1,2 +1,7 @@
 package ratetracker
 
+// TODO: Jaser
+// here we want a struct with methods and store value/metadata
+// maybe we want to know total API_KEYS we are tracking...etc
+// This is where our mapping of KEY : ratetrackerInstance will be
+// Our Rate module will be calling with this module
